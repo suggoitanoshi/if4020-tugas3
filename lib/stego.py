a40f4f4d0e7a118ba4c2613397b11fe8e95f001f
Fransiskus Febryan Suryawan <franfebryan@protonmail.com>
2021-09-30 20:21:21 +0700

    fix: remove unused try/catch block

--- lib/stego.py
+++ lib/stego.py
@@ -57,153 +57,146 @@ class image_stego(stego):
     return org_value
   
   def embed(self, outputfile: str, message: bytearray, key: bytearray, encryption: bool, randomize: bool):
-    try:
-      target_image = Image.open(self.input_file,'r')
+    target_image = Image.open(self.input_file,'r')
 
-      # Get pixel array
-      pixel_array = list(target_image.copy().getdata())
-      pixel_array_new = pixel_array.copy()
-      pixel_tuple_length = 1
+    # Get pixel array
+    pixel_array = list(target_image.copy().getdata())
+    pixel_array_new = pixel_array.copy()
+    pixel_tuple_length = 1
 
-      # Get image info
-      image_pixel_type = target_image.mode    
+    # Get image info
+    image_pixel_type = target_image.mode    
+    
+    if image_pixel_type not in ["1","L","P"]: 
+      # Non mono color info and converting array of tuples
+      pixel_tuple_length = len(pixel_array_new[0])
+      pixel_array_new = [item for t in pixel_array_new for item in t]
+    image_message_capacity = len(pixel_array_new) - self.MESSAGE_INFO_LENGTH
+
+    if image_message_capacity <= 0:
+      raise Exception("FAIL : EMPTY IMAGE PIXELS")
+
+    # Message length check
+    if len(message)*8 <= 0 or len(message)*8 > image_message_capacity:
+      raise Exception("FAIL : MESSAGE CAPACITY")
+    
+    # Message encrypting
+    if encryption:
+      rc = RC4(key)
+      enc_message = self.ba_to_bitstring(rc.encrypt(message))
+    else:
+      enc_message = self.ba_to_bitstring(message)
+
+    # Creating message header
+    message_info_bin = "1" if randomize else "0"        # Random mark
+    message_info_bin += "1" if encryption else "0"      # Encryption mark
+    message_info_bin += format(len(enc_message),'032b') # Message Length mark
+
+    # Embedding message header
+    for i in range(len(message_info_bin)):
+      pixel_array_new[i] = self.lsb_replace(pixel_array_new[i],message_info_bin[i])
+
+    # Embedding message
+    if randomize:
+      random.seed(key)
       
-      if image_pixel_type not in ["1","L","P"]: 
-        # Non mono color info and converting array of tuples
-        pixel_tuple_length = len(pixel_array_new[0])
-        pixel_array_new = [item for t in pixel_array_new for item in t]
-      image_message_capacity = len(pixel_array_new) - self.MESSAGE_INFO_LENGTH
+      history = []
+      for i in range(len(enc_message)):
+        gen_idx = self.MESSAGE_INFO_LENGTH + random.randint(0,image_message_capacity - 1)
 
-      if image_message_capacity <= 0:
-        raise Exception("FAIL : EMPTY IMAGE PIXELS")
-
-      # Message length check
-      if len(message)*8 <= 0 or len(message)*8 > image_message_capacity:
-        raise Exception("FAIL : MESSAGE CAPACITY")
-      
-      # Message encrypting
-      if encryption:
-        rc = RC4(key)
-        enc_message = self.ba_to_bitstring(rc.encrypt(message))
-      else:
-        enc_message = self.ba_to_bitstring(message)
-
-      # Creating message header
-      message_info_bin = "1" if randomize else "0"        # Random mark
-      message_info_bin += "1" if encryption else "0"      # Encryption mark
-      message_info_bin += format(len(enc_message),'032b') # Message Length mark
-
-      # Embedding message header
-      for i in range(len(message_info_bin)):
-        pixel_array_new[i] = self.lsb_replace(pixel_array_new[i],message_info_bin[i])
-
-      # Embedding message
-      if randomize:
-        random.seed(key)
-        
-        history = []
-        for i in range(len(enc_message)):
+        while gen_idx in history:
           gen_idx = self.MESSAGE_INFO_LENGTH + random.randint(0,image_message_capacity - 1)
 
-          while gen_idx in history:
-            gen_idx = self.MESSAGE_INFO_LENGTH + random.randint(0,image_message_capacity - 1)
-
-          history.append(gen_idx)
-          
-          pixel_array_new[gen_idx] = self.lsb_replace(pixel_array_new[gen_idx],enc_message[i])
-      else:
-        for i in range(len(enc_message)):
-          pixel_array_new[self.MESSAGE_INFO_LENGTH + i] = self.lsb_replace(pixel_array_new[self.MESSAGE_INFO_LENGTH + i],enc_message[i])
+        history.append(gen_idx)
         
+        pixel_array_new[gen_idx] = self.lsb_replace(pixel_array_new[gen_idx],enc_message[i])
+    else:
+      for i in range(len(enc_message)):
+        pixel_array_new[self.MESSAGE_INFO_LENGTH + i] = self.lsb_replace(pixel_array_new[self.MESSAGE_INFO_LENGTH + i],enc_message[i])
       
-      # PSNR check
-      if image_pixel_type in ["1","L","P"]:
-        if image_pixel_type == "1":
-          for i in range(len(pixel_array_new)):
-            if pixel_array_new[i] == 255 - 1: # originally 1, replaced by 0
-              pixel_array_new[i] = 0
-            elif pixel_array_new[i] == 0 + 1: # originally 0, replaced by 1
-              pixel_array_new[i] = 255
-        sigma_i = 0
-        for i in range(len(pixel_array)):
-          sigma_i += (pixel_array[i]-pixel_array_new[i]) ** 2
-        rms = sqrt(sigma_i/(target_image.size[0]*target_image.size[1]))
-        psnr = 20 * log10(255/rms)
-        if psnr <= 30: # PSNR too low
+    
+    # PSNR check
+    if image_pixel_type in ["1","L","P"]:
+      if image_pixel_type == "1":
+        for i in range(len(pixel_array_new)):
+          if pixel_array_new[i] == 255 - 1: # originally 1, replaced by 0
+            pixel_array_new[i] = 0
+          elif pixel_array_new[i] == 0 + 1: # originally 0, replaced by 1
+            pixel_array_new[i] = 255
+      sigma_i = 0
+      for i in range(len(pixel_array)):
+        sigma_i += (pixel_array[i]-pixel_array_new[i]) ** 2
+      rms = sqrt(sigma_i/(target_image.size[0]*target_image.size[1]))
+      psnr = 20 * log10(255/rms)
+      if psnr <= 30: # PSNR too low
+        raise Exception("FAIL : PSNR < 30 : PSNR = " + str(psnr))
+    else:
+      it = [iter(pixel_array_new)] * pixel_tuple_length
+      pixel_array_new = list(zip(*it))
+
+      sigma_i = [0 for i in range(pixel_tuple_length)]
+      for pixel_idx in range(len(pixel_array)):
+        for channel in range(len(pixel_array[pixel_idx])):
+          sigma_i[channel] += (pixel_array[pixel_idx][channel]-pixel_array_new[pixel_idx][channel]) ** 2
+      rms = [(sqrt(sigma_i_channel/(target_image.size[0]*target_image.size[1]))) for sigma_i_channel in sigma_i]
+
+      psnr = ""
+      
+      for channel_rms in rms:
+        psnr_c = 20 * log10(255/channel_rms)
+        psnr += str(psnr_c) + " "
+        if psnr_c <= 30: # PSNR too low
           raise Exception("FAIL : PSNR < 30 : PSNR = " + str(psnr))
-      else:
-        it = [iter(pixel_array_new)] * pixel_tuple_length
-        pixel_array_new = list(zip(*it))
-
-        sigma_i = [0 for i in range(pixel_tuple_length)]
-        for pixel_idx in range(len(pixel_array)):
-          for channel in range(len(pixel_array[pixel_idx])):
-            sigma_i[channel] += (pixel_array[pixel_idx][channel]-pixel_array_new[pixel_idx][channel]) ** 2
-        rms = [(sqrt(sigma_i_channel/(target_image.size[0]*target_image.size[1]))) for sigma_i_channel in sigma_i]
-
-        psnr = ""
-        
-        for channel_rms in rms:
-          psnr_c = 20 * log10(255/channel_rms)
-          psnr += str(psnr_c) + " "
-          if psnr_c <= 30: # PSNR too low
-            raise Exception("FAIL : PSNR < 30 : PSNR = " + str(psnr))
-      
-      # Save result
-      created_image = Image.new(mode=target_image.mode, size=target_image.size)
-      created_image.putdata(pixel_array_new)
-      created_image.save(outputfile)
-    except Exception as e:
-      print(e)
+    
+    # Save result
+    created_image = Image.new(mode=target_image.mode, size=target_image.size)
+    created_image.putdata(pixel_array_new)
+    created_image.save(outputfile)
 
     
   def extract(self, key: bytearray) -> bytearray:
-    try:
-      target_image = Image.open(self.input_file,'r')
+    target_image = Image.open(self.input_file,'r')
 
-      # Pixel data check
-      if target_image.size[0] == 0 or target_image.size[1] == 0:
-        raise Exception("FAIL : NO PIXEL DATA FOUND")
-      
-      # Get pixel array
-      pixel_array = list(target_image.copy().getdata())
-      if target_image.mode not in ["1","L","P"]:
-        pixel_array = [item for t in pixel_array for item in t]
-      # Get message info
-      message_info = ""
-      for i in range(self.MESSAGE_INFO_LENGTH):
-        message_info += "1" if pixel_array[i] % 2 == 1 else "0"
-      randomize = bool(int(message_info[0]))
-      encryption = bool(int(message_info[1]))
-      message_length = int(message_info[2:],2)
+    # Pixel data check
+    if target_image.size[0] == 0 or target_image.size[1] == 0:
+      raise Exception("FAIL : NO PIXEL DATA FOUND")
+    
+    # Get pixel array
+    pixel_array = list(target_image.copy().getdata())
+    if target_image.mode not in ["1","L","P"]:
+      pixel_array = [item for t in pixel_array for item in t]
+    # Get message info
+    message_info = ""
+    for i in range(self.MESSAGE_INFO_LENGTH):
+      message_info += "1" if pixel_array[i] % 2 == 1 else "0"
+    randomize = bool(int(message_info[0]))
+    encryption = bool(int(message_info[1]))
+    message_length = int(message_info[2:],2)
 
-      # Calculate max idx
-      image_message_capacity = len(pixel_array) - self.MESSAGE_INFO_LENGTH
+    # Calculate max idx
+    image_message_capacity = len(pixel_array) - self.MESSAGE_INFO_LENGTH
 
-      # Extracting binary
-      message_bin = ""
-      if randomize:
-        history = []
-        random.seed(key)
-        for i in range(message_length):
+    # Extracting binary
+    message_bin = ""
+    if randomize:
+      history = []
+      random.seed(key)
+      for i in range(message_length):
+        gen_idx = self.MESSAGE_INFO_LENGTH + random.randint(0,image_message_capacity - 1)
+
+        while gen_idx in history:
           gen_idx = self.MESSAGE_INFO_LENGTH + random.randint(0,image_message_capacity - 1)
 
-          while gen_idx in history:
-            gen_idx = self.MESSAGE_INFO_LENGTH + random.randint(0,image_message_capacity - 1)
+        history.append(gen_idx)
 
-          history.append(gen_idx)
-
-          message_bin += "1" if pixel_array[gen_idx] % 2 == 1 else "0"
-      else:
-        for i in range(message_length):
-          message_bin += "1" if pixel_array[self.MESSAGE_INFO_LENGTH+i] % 2 == 1 else "0"
-      
-      # Returning extracted message
-      if encryption:
-        rc = RC4(key)                
-        return rc.decrypt(self.bitstring_to_ba(message_bin))
-      else:
-        return self.bitstring_to_ba(message_bin)
+        message_bin += "1" if pixel_array[gen_idx] % 2 == 1 else "0"
+    else:
+      for i in range(message_length):
+        message_bin += "1" if pixel_array[self.MESSAGE_INFO_LENGTH+i] % 2 == 1 else "0"
     
-    except Exception as e:
-      print(e)
+    # Returning extracted message
+    if encryption:
+      rc = RC4(key)                
+      return rc.decrypt(self.bitstring_to_ba(message_bin))
+    else:
+      return self.bitstring_to_ba(message_bin)
